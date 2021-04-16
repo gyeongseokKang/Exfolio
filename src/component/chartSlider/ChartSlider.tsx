31aa9e30159c0e0088e1fe03315aa6615fc9c172
gyeongseokKang <kyeungseok_kang@tmax.co.kr>
2021-04-16 19:07:13 +0900

    [Update] 비중 변경 컴포넌트
    - 구획화 및 구조 변경

--- src/component/chartSlider/ChartSlider.tsx
+++ src/component/chartSlider/ChartSlider.tsx
@@ -22,32 +22,86 @@ function ChartSlider() {
     setSharesHeldList(updateList);
   };
 
+  let width = 500;
+  let height = 500;
+
   return (
     <>
-      <Plot
-        data={[
-          {
-            labels: sharesHeldList.map((item) => item.name),
-            values: sharesHeldList.map((item) => item.radio),
-            type: "pie",
-            hoverinfo: "label+percent",
-            textinfo: "label+percent",
-          },
-        ]}
-        layout={{ width: 500, height: 500 }}
-        config={{ displayModeBar: false }}
-      />
-      <div style={{ paddingLeft: "10px", width: 490 }}>
-        {sharesHeldList.map((item) => {
-          return (
-            <RatioSlider
-              name={item.name}
-              value={item.radio}
-              key={item.name}
-              onChange={onChange}
-            />
-          );
-        })}
+      <div
+        style={{
+          width: "800px",
+          height: "500px",
+          borderColor: "black",
+          borderWidth: "2px",
+          borderStyle: "solid",
+          display: "flex",
+        }}
+      >
+        <div
+          style={{
+            width: "500px",
+            height: "350px",
+            borderColor: "black",
+            borderWidth: "2px",
+            borderStyle: "solid",
+          }}
+        >
+          <Plot
+            data={[
+              {
+                labels: sharesHeldList.map((item) => item.name),
+                values: sharesHeldList.map((item) => item.radio),
+                type: "pie",
+                hoverinfo: "label+percent",
+                textinfo: "label+percent",
+                sort: false,
+              },
+            ]}
+            layout={{
+              width: 500,
+              height: 500,
+              showlegend: false,
+              title: "<추천 포트폴리오>",
+            }}
+            config={{ displayModeBar: false }}
+          />
+        </div>
+        <div
+          style={{
+            padding: "0px 10px 0px 10px",
+            borderColor: "black",
+            borderWidth: "2px",
+            borderStyle: "solid",
+          }}
+        >
+          [ 종목구성 ]
+          {sharesHeldList.map((item) => {
+            return (
+              <RatioSlider
+                name={item.name}
+                value={item.radio}
+                key={item.name}
+                onChange={onChange}
+              />
+            );
+          })}
+        </div>
+      </div>
+      <div
+        style={{
+          width: "800px",
+          height: "150px",
+          borderColor: "black",
+          borderWidth: "2px",
+          borderStyle: "solid",
+        }}
+      >
+        이곳은 모델 설명 텍스트를 박는곳이곳은 모델 설명 텍스트를 박는곳이곳은
+        모델 설명 텍스트를 박는곳이곳은 모델 설명 텍스트를 박는곳이곳은 모델
+        설명 텍스트를 박는곳이곳은 모델 설명 텍스트를 박는곳이곳은 모델 설명
+        텍스트를 박는곳이곳은 모델 설명 텍스트를 박는곳이곳은 모델 설명 텍스트를
+        박는곳이곳은 모델 설명 텍스트를 박는곳이곳은 모델 설명 텍스트를
+        박는곳이곳은 모델 설명 텍스트를 박는곳
       </div>
     </>
   );

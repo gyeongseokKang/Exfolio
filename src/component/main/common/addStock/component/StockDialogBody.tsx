619c62cf0cbc23a79fdd229bbc97311349b9cc19
gyeongseokKang <kyeungseok_kang@tmax.co.kr>
2021-05-21 18:52:26 +0900

    [Refactor] 주식 검색 컴포넌트 분리
    - 코드 리펙토링

--- src/component/main/common/addStock/component/StockDialogBody.tsx
+++ src/component/main/common/addStock/component/StockDialogBody.tsx
@@ -86,60 +86,73 @@ const StockDialogBody = ({ searchQuery, onAdd, onDelete, matchedStocks }: StockD
         </div>
         <div style={{ overflowY: "scroll", overflowX: "hidden", maxHeight: "500px" }}>
           {matchedStocks.map((StockData: { code: string; name: string; checked: boolean }) => (
-            <div
-              className={classes.stockItem}
-              key={StockData.code + "layout"}
-              onClick={(e: any) => {
-                if (e.target.innerText.length > 0) {
-                  const targetStock = matchedStocks.find((item) => item.name === e.target.innerText || item.code === e.target.innerText);
-                  if (targetStock) {
-                    onAdd(targetStock.name, targetStock.code);
-                  }
-                }
-              }}
-            >
-              <div className={classes.stockCode} id={`${StockData.code}_code`}>
-                {StockData.code.includes(searchQuery) ? (
-                  <>
-                    {StockData.code.split(searchQuery)[0]}
-                    <span style={{ color: "#3F51B5" }}>{searchQuery}</span>
-                    {StockData.code.split(searchQuery)[1]}
-                  </>
-                ) : (
-                  StockData.code
-                )}
-              </div>
-              <div className={classes.stockName} id={`${StockData.name}_name`}>
-                {StockData.name.includes(searchQuery) ? (
-                  <>
-                    {StockData.name.split(searchQuery)[0]}
-                    <span style={{ color: "#3F51B5" }}>{searchQuery}</span>
-                    {StockData.name.split(searchQuery)[1]}
-                  </>
-                ) : (
-                  StockData.name
-                )}
-              </div>
-              <Checkbox
-                id={`${StockData.code}_checkbox`}
-                checked={StockData.checked}
-                color="primary"
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                  const targetStock = matchedStocks.find((item) => item.code === e.target.id.split("_")[0]);
-                  if (targetStock === undefined) return;
-                  if (e.target.checked) {
-                    onAdd(targetStock.name, targetStock.code);
-                  } else {
-                    onDelete(targetStock.name);
-                  }
-                }}
-              />
-            </div>
+            <BodyItem StockData={StockData} searchQuery={searchQuery} onAdd={onAdd} onDelete={onDelete} matchedStocks={matchedStocks} />
           ))}
           {matchedStocks.length === 0 ? <div className={classes.noMatchedText}>검색된 결과가 없습니다.</div> : undefined}
         </div>
       </div>
     </>
+  );
+};
+
+interface BodyItemProp {
+  searchQuery: string;
+  onAdd(name: string, code: string): void;
+  onDelete(name: string): void;
+  matchedStocks: {
+    code: string;
+    name: string;
+    checked: boolean;
+  }[];
+  StockData: { code: string; name: string; checked: boolean };
+}
+
+const BodyItem = ({ StockData, searchQuery, onAdd, onDelete, matchedStocks }: BodyItemProp) => {
+  const classes = useStyles();
+
+  const bodyRowClicked = (e: any) => {
+    if (e.target.innerText.length > 0) {
+      const targetStock = matchedStocks.find((item) => item.name === e.target.innerText || item.code === e.target.innerText);
+      if (targetStock) {
+        onAdd(targetStock.name, targetStock.code);
+      }
+    }
+  };
+  const onCheckboxClicked = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const targetStock = matchedStocks.find((item) => item.code === e.target.id.split("_")[0]);
+    if (targetStock === undefined) return;
+    if (e.target.checked) {
+      onAdd(targetStock.name, targetStock.code);
+    } else {
+      onDelete(targetStock.name);
+    }
+  };
+  return (
+    <div className={classes.stockItem} key={StockData.code + "layout"} onClick={bodyRowClicked}>
+      <div className={classes.stockCode} id={`${StockData.code}_code`}>
+        {StockData.code.includes(searchQuery) ? (
+          <>
+            {StockData.code.split(searchQuery)[0]}
+            <span style={{ color: "#3F51B5" }}>{searchQuery}</span>
+            {StockData.code.split(searchQuery)[1]}
+          </>
+        ) : (
+          StockData.code
+        )}
+      </div>
+      <div className={classes.stockName} id={`${StockData.name}_name`}>
+        {StockData.name.includes(searchQuery) ? (
+          <>
+            {StockData.name.split(searchQuery)[0]}
+            <span style={{ color: "#3F51B5" }}>{searchQuery}</span>
+            {StockData.name.split(searchQuery)[1]}
+          </>
+        ) : (
+          StockData.name
+        )}
+      </div>
+      <Checkbox id={`${StockData.code}_checkbox`} checked={StockData.checked} color="primary" onChange={onCheckboxClicked} />
+    </div>
   );
 };
 

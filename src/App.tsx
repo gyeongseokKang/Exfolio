c3e51a19a285cb0b9d99b05a7d42472db1aac3b0
gyeongseokKang <kyeungseok_kang@tmax.co.kr>
2021-07-28 22:36:40 +0900

    [New feature] 코드 스플리팅 적용
    - lazy import를 통한 코드 분할
    - github page 권고 25mb 이하로 분할하기 위함

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Route, Switch } from "react-router-dom";
 import Main from "./component/main/Main";
 import LNB from "./component/lnb/LNB";
-import CreatePage from "./component/main/createPage/CreatePage";
-import AnalysisPage from "./component/main/analysisPage/AnalysisPage";
-import PropensityPage from "./component/main/propensity/PropensityPage";
-import PerformancePage from "./component/main/performancePage/PerformancePage";
 import { LoginProvider } from "./contexts/LoginContext";
 
 function App() {
+  const CreatePage = React.lazy(() => import("./component/main/createPage/CreatePage"));
+  const AnalysisPage = React.lazy(() => import("./component/main/analysisPage/AnalysisPage"));
+  const PropensityPage = React.lazy(() => import("./component/main/propensity/PropensityPage"));
+  const PerformancePage = React.lazy(() => import("./component/main/performancePage/PerformancePage"));
   return (
     <>
       <LoginProvider>
-        <LNB />
-        <Switch>
-          <Route exact path="/" component={Main}></Route>
-          <Route path="/create" component={CreatePage}></Route>
-          <Route path="/analysis" component={AnalysisPage}></Route>
-          <Route path="/propensity" component={PropensityPage}></Route>
-          <Route path="/performance" component={PerformancePage}></Route>
-        </Switch>
+        <Suspense fallback={<div>Loading...</div>}>
+          <LNB />
+          <Switch>
+            <Route exact path="/" component={Main}></Route>
+            <Route path="/create" component={CreatePage}></Route>
+            <Route path="/analysis" component={AnalysisPage}></Route>
+            <Route path="/propensity" component={PropensityPage}></Route>
+            <Route path="/performance" component={PerformancePage}></Route>
+          </Switch>
+        </Suspense>
       </LoginProvider>
     </>
   );
